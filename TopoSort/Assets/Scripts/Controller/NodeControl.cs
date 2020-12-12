524a377b0dd5ee3daed3ed7196214c6c1d80f7b1
Clonetrash <jamison.bassett@gmx.de>
2020-12-12 15:20:05 +0100

    AlgoDat2 implemented drag and drop of Nodes

---
+++ TopoSort/Assets/Scripts/Controller/NodeControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeControl : MonoBehaviour
+{
+
+    private float startPosX;
+    private float startPosY;
+    private bool isHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isHeld)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+        
+    }
+
+    private void OnMouseDown() 
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isHeld = true;
+
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        isHeld = false;
+    }
+
+}
