f76dc0b918a14976d324404cc4f16bd4ef65b7e4
Oliver <oboeer@gmail.com>
2021-02-02 19:22:07 +0100

    Outsourced tutorial and glossary texts
    added TextLoader class (which loads texts from xml)

--- TopoSort/Assets/Scripts/GlossarCanvas.cs
+++ TopoSort/Assets/Scripts/GlossarCanvas.cs
@@ -49,80 +49,36 @@ public class GlossarCanvas : MonoBehaviour
     {
         GlossarEntry entryGerman;
         GlossarEntry entryEnglish;
-        
-        
-        
+        TextLoader germanLoader = new TextLoader("Assets\\Scripts\\xml\\Glossar_deutsch.xml");
+        TextLoader englishLoader = new TextLoader("Assets\\Scripts\\xml\\Glossar_englisch.xml");
+
+
         // 0 - Graph Entry
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Graph");
-        entryGerman.GetSynonyms().Add("Digraph (gerichtet)");
-        entryGerman.SetExplanation("Ein Graph ist eine aus Knoten und Kanten bestehende Struktur.\n" +
-                                   "Weisen alle Kanten eine Orientierung auf, so ist dieser Graph \"gerichtet\"");
-        
-        entryEnglish.SetTitle("Graph");
-        entryEnglish.GetSynonyms().Add("Digraph (directed)");
-        entryEnglish.SetExplanation("A mathematical structure consisting of nodes and edges\n" +
-                                   "If every edge has an orientation the Graph is called \"directed\"");
-        
+        entryGerman = germanLoader.LoadGlossarEntry(0);
+        entryEnglish = englishLoader.LoadGlossarEntry(0);
+
         EntriesGerman.Add(TermsGerman[0], entryGerman);
         EntriesEnglish.Add(TermsGerman[0], entryEnglish);
         
         
         // 1 - Vertex Entry
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Knoten");
-        entryGerman.GetSynonyms().Add("Vertex");
-        entryGerman.SetExplanation("Bestandteil eines Graphen, welcher meißt einen Namen oder einen Wert besitzt\n");
-        
-        entryEnglish.SetTitle("Node");
-        entryEnglish.GetSynonyms().Add("Vertex");
-        entryEnglish.SetExplanation("A part of a graph. Usually it has a name or value associated with it.\n");
+        entryGerman = germanLoader.LoadGlossarEntry(1);
+        entryEnglish = englishLoader.LoadGlossarEntry(1);
         
         EntriesGerman.Add(TermsGerman[1], entryGerman);
         EntriesEnglish.Add(TermsGerman[1], entryEnglish);
         
         
         // 2 - Edge Entry
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Kante");
-        entryGerman.GetSynonyms().Add("Bogen (gerichtet)");
-        entryGerman.SetExplanation("Ein Bestandteil eines Graphen, welcher zwei Knoten miteinander verbindet\n" +
-                                   "Eine Kante, die eine eindeutige Richtung aufweist (eine Orientierung hat)\n" +
-                                   "nennt man \"gerichtet\" und werden meist als \"Bogen\" bezeichnet");
-        
-        entryEnglish.SetTitle("Edge");
-        entryEnglish.GetSynonyms().Add("Arc (directed)");
-        entryEnglish.GetSynonyms().Add("Arrow (directed)");
-        entryEnglish.SetExplanation("A part of a graph, which connects two nodes with each other\n" +
-                                    "An edge that has a defined orientation is called \"directed\" and is often referred to as an \"arc\"");
-        
+        entryGerman = germanLoader.LoadGlossarEntry(2);
+        entryEnglish = englishLoader.LoadGlossarEntry(2);
         EntriesGerman.Add(TermsGerman[2], entryGerman);
         EntriesEnglish.Add(TermsGerman[2], entryEnglish);
         
         
         // 3 - Degree Entry
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Knotengrad");
-        entryGerman.SetExplanation("Anzahl aller Kanten, die inzident (anliegend) an einen Knoten sind.\n" +
-                                   "\n" +
-                                   "In einem gerichteten Graphen:\n" +
-                                   "- Die Anzahl der Bögen, die in einen Knoten eingehen nennt man \"Innengrad\"\n" +
-                                   "- Die Anzahl der Bögen, die von einem Knoten ausgehen nennt man \"Außengrad\"");
-        
-        entryEnglish.SetTitle("Degree");
-        entryEnglish.SetExplanation("The number of edges that are incident to a node\n" +
-                                   "\n" +
-                                   "In a directed graph:\n" +
-                                   "- The number of arcs entering a node is called \"in-degree\"\n" +
-                                   "- The number of arcs exiting a node is called \"out-degree\"");
+        entryGerman = germanLoader.LoadGlossarEntry(3);
+        entryEnglish = englishLoader.LoadGlossarEntry(3);
         
         EntriesGerman.Add(TermsGerman[3], entryGerman);
         EntriesEnglish.Add(TermsGerman[3], entryEnglish);
@@ -130,20 +86,8 @@ public class GlossarCanvas : MonoBehaviour
         
         
         // 4 - Sorting Entry
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Sortierung");
-        entryGerman.SetExplanation("Eine mit einer bestimmten Ordnung versehenen Folge von Objekten.\n" +
-                                   "\n" +
-                                   "Topologische Sortierung:\n" +
-                                   "Eine Sortierung, welche Objekte nach ihren Abhängigkeiten zueinander ordnet.");
-        
-        entryEnglish.SetTitle("Sorting");
-        entryEnglish.SetExplanation("A sequence of objects in a certain order.\n" +
-                                    "\n" +
-                                    "topological sort:" +
-                                    "A sorting which sorts objects according to their dependencies on each other.");
+        entryGerman = germanLoader.LoadGlossarEntry(4);
+        entryEnglish = englishLoader.LoadGlossarEntry(4);
         
         EntriesGerman.Add(TermsGerman[4], entryGerman);
         EntriesEnglish.Add(TermsGerman[4], entryEnglish);
@@ -151,14 +95,8 @@ public class GlossarCanvas : MonoBehaviour
         
         
         // 5 - Algorithm
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Algorithmus");
-        entryGerman.SetExplanation("Endliche Folge von Handlungsschritten zur Lösung eines Problems oder einer Problemklasse.\n");
-        
-        entryEnglish.SetTitle("Algorithmn");
-        entryEnglish.SetExplanation("A finite sequence of steps to solve a problem or a problem class");
+        entryGerman = germanLoader.LoadGlossarEntry(5);
+        entryEnglish = englishLoader.LoadGlossarEntry(5);
         
         EntriesGerman.Add(TermsGerman[5], entryGerman);
         EntriesEnglish.Add(TermsGerman[5], entryEnglish);
@@ -166,18 +104,8 @@ public class GlossarCanvas : MonoBehaviour
         
         
         // 6 - Kahn Algorithm
-        entryGerman = new GlossarEntry();
-        entryEnglish = new GlossarEntry();
-        
-        entryGerman.SetTitle("Kahn-Algorithmus");
-        entryGerman.SetExplanation("Algorithmus, der ein einem azyklischen, gerichteten Graphen eine topologische Sortierung auf den Knoten ausführt.\n" +
-                                   "Ein Knoten wird immer nur dann in die Ausgabe geschrieben, wenn dessen Vorgänger bereits abgearbeitet wurden.\n" +
-                                   "(Und der Algorithmus, der diese Simulation antreibt.)");
-        
-        entryEnglish.SetTitle("Kahn's Algorithmn");
-        entryEnglish.SetExplanation("An algorithm that performs a topological sorting on the nodes of an acyclic, directed graph.\n" +
-                                    "A node is only written to the output if its ancestor has already been processed.\n" +
-                                    "(and the algorithm that drives this simulation.)");
+        entryGerman = germanLoader.LoadGlossarEntry(6);
+        entryEnglish = englishLoader.LoadGlossarEntry(6);
         
         EntriesGerman.Add(TermsGerman[6], entryGerman);
         EntriesEnglish.Add(TermsGerman[6], entryEnglish);
---
+++ TopoSort/Assets/Scripts/TextLoader.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+using TopoSort;
+
+namespace TopoSort
+{
+    public class TextLoader
+    {
+        private string Path;  
+        public TextLoader(string path) 
+        {
+            this.Path = path;
+        }
+
+        
+        public string LoadTutorialPage(int page) 
+        {
+            String text = "";   //will contain the page number, headline and text
+            int count = 0;      //if a node attribute match page, it will increased by 3 
+            XmlTextReader xtr = new XmlTextReader(this.Path);
+
+            while (xtr.Read())
+            {
+                String attr = xtr.GetAttribute("number");
+                if (attr != null)
+                {
+                    if (attr.Equals(page.ToString()))
+                    {
+                        text = text + "Page: " + attr + "\r\n";
+                        count += 3;
+                        continue;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                if (count != 0 && count > 0) 
+                {
+                    if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "headline")
+                    {
+                        text = text + xtr.ReadElementString() + "\r\n";
+                    }
+
+                    if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "body")
+                    {
+                        text = text + xtr.ReadElementString() + "\r\n";
+                    }
+                    count--;
+                }
+            }
+            return text;
+        }
+
+        public GlossarEntry LoadGlossarEntry(int id) 
+        {
+            GlossarEntry entry = new GlossarEntry();
+            XmlTextReader xtr = new XmlTextReader(this.Path);
+            int count = 0;
+            while (xtr.Read()) 
+            {
+
+                String attr = xtr.GetAttribute("id");
+                if (attr != null)
+                {
+                    if (attr.Equals(id.ToString()))
+                    {
+                        count += 4;
+                        continue;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                if(count != 0)
+                {
+                    //loading terms
+                    if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "term")
+                    {
+                        entry.SetTitle(xtr.ReadElementString());
+                    }
+
+                    // loading synonyms
+                    if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "synonyms")
+                    {
+                        List<string> list = new List<string>();
+                        String syn = xtr.ReadElementString();
+                        if (!syn.Equals(String.Empty))
+                        {
+                            if (syn.IndexOf(',') == 0)
+                            {
+                                list.Add(syn);
+                                break;
+                            }
+
+                            while (syn.IndexOf(',') > 0)
+                            {
+                                int index = syn.IndexOf(',');
+                                String sub = syn.Substring(0, index);
+                                list.Add(sub);
+                                syn = syn.Remove(0, index + 1);
+                            }
+
+                            list.Add(syn);
+                        }
+
+                        entry.SetSynonyms(list);
+                    }
+
+                    //loading defintion
+                    if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "definition")
+                    {
+                       entry.SetExplanation(xtr.ReadElementString());
+                    }
+                    count--;
+                }
+            }
+
+            return entry;
+        }
+
+        public void ChangePath(string path)
+        {
+            this.Path = path;
+        }
+         
+    }
+}
