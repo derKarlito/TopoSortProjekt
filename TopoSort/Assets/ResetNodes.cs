ccc6e2cbd9d1d9ccff95dc8b8845393d13ee53e5
Clonetrash <jamison.bassett@gmx.de>
2021-02-03 12:40:51 +0100

    Done and tested through

---
+++ TopoSort/Assets/ResetNodes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TopoSort;
+
+public class ResetNodes : MonoBehaviour
+{
+    private Collider2D Collider;
+    public static GameObject[] Nodes;
+    public Algorithm Algorithm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider = GetComponentInChildren<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool onButton = MouseManager.MouseHover(Collider);
+        if(Input.GetMouseButtonDown(0) && onButton)
+        {
+            DeleteAllNodes();
+            Algorithm.ResetGraph();
+        }
+    }
+    
+    public void DeleteAllNodes()
+    {
+        Nodes = GameObject.FindGameObjectsWithTag("Node"); //Gets all nodes on screen
+
+        foreach (GameObject node in Nodes)  //iterates through every node
+        {
+            NodeControl nodeControl = node.GetComponent<NodeControl>();
+            nodeControl.DeleteNode();
+        }
+    }
+}
